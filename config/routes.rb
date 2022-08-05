1c97b382123c5db099b57fdb4d0a9d852f6d037a
Patricia <patriciaespejoq@gmail.com>
2022-08-05 18:10:46 -0400

    ultimo gran commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :stories
+  resources :users, only: %i[ new create show] 
+  resources :sessions, only: %i[ new create destroy] 
 
   root 'stories#index'
 end
